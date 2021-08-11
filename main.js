f2f2f39b27efd14a5f78549317868b79370717c0
Akshara-Dhoble <88190894+Akshara-Dhoble@users.noreply.github.com>
2021-08-11 14:42:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log("My first Console Project");
+var firstname = "Akshara";
+var lastname = "Dhoble";
+var fullname = firstname.concat(lastname);
+console.log(fullname);
+
+var a = 19;
+var b = 17;
+
+var add = a + b ;
+console.log(add);
+
+var c = 29;
+var d = 27;
+
+var sub = c - d;    
+console.log(sub);
+
+var e = 18;
+var f = 12;
+
+var multiply = e * f;
+console.log(multiply);
+
+var g = 14;
+var h = 6;
+
+var div = g / h;
+console.log(div);
